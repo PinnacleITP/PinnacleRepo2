d336c8164a5bbbc8abf30ae9ee23bcea24466d01
dasunhimsara <110025589+dasunhimsara@users.noreply.github.com>
2024-04-22 05:11:57 +0530

    user profile management
    consider
    src./lib folder
    src./index.js
    
    created pages
    created userprofile
    pdf generation
    register
    login
    search bar

--- ITPProject/pinnacle/src/pages/Myaccount.js
+++ ITPProject/pinnacle/src/pages/Myaccount.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import axios from "axios";
 import { HashLoader } from "react-spinners";
 import DeleteWorning from "../assets/payment/deleteanimation.webm";
@@ -15,6 +15,10 @@ import Payment_history_card from "../components/Payment_history_card";
 import Game_download_card from "../components/Game_download_card";
 import Channel from "../components/Channel";
 import Footer from "../components/Footer";
+import useSWR from 'swr';
+import { useNavigate } from 'react-router-dom';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 export default function Myaccount() {
   var memberID ="66118d9104fb9c92e1c7d980";
@@ -38,6 +42,22 @@ export default function Myaccount() {
   const handleDeleteCloseSuccessPopup = () => {
     setDeleteSuccessMessagechecked(false);
   };
+  {/*-------my profile----*/}
+  const inputClasses = "bg-zinc-700 border-b border-orange-600 text-white p-2 w-full mt-3";
+  const buttonClasses = "text-white font-bold py-2 px-4 bg-gradient-to-r from-[#FE7804] to-[#FF451D] rounded-lg";
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+
+  const [profileImage, setProfileImage] = useState(null);
+
+  const [allUserData, setAllUserData] = useState([]);
+
+  const [userData, setUserData] = useState('');
+  const [reloadCount, setReloadCount] = useState(0);
+
+  const navigate = useNavigate();
+  const imageInputRef = useRef(null);
 
   {
     /* ##################################################################### Payment management ######################################*/
@@ -148,21 +168,305 @@ export default function Myaccount() {
       })
       .catch((err) => console.log(err));
   };
+  {
+    /* ################################################################### User management ######################################*/
+  }
+
+  const getCurrentDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let day = today.getDate();
+
+    // Add leading zero if month or day is less than 10
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+
+    return `${year}-${month}-${day}`;
+  };
+
+  // const handleDivClick = (divId) => {
+  //   setSelectedDiv(divId === selectedDiv ? null : divId);
+  // };
+
+  // const handleChannel = (divId) => {
+  //   setChannelDiv(divId === channelDiv ? null : divId);
+  // };
+
+  const onClickChangePass = async () => {
+    const passValue = document.getElementById('newPassword').value;
+    const confirmPassValue = document.getElementById('confirmPassword').value;
+
+    if(passValue == '' || confirmPassValue == ''){
+      alert('Fields cannot be empty.');
+    }else if (passValue != confirmPassValue){
+      alert('Passwords not matched. Please Check.')
+    }else{
+      let formData = {
+        username : data?.user.username,
+        email : data?.user.email,
+        newPass : passValue,
+        detail : "changePass"
+      }
+      try {
+        const response = await axios.put(
+          'http://localhost:3001/api/changeData',
+          formData,
+        );
+        console.log('Response:', response.data);
+        alert("Password Changed Successfully");
+        setTimeout(() => {
+          navigate('/login');
+        }, 1000);
+      } catch (error) {
+        alert( error.response.data.error);
+        console.error('Error:', error);
+      }
+    }
+  }
+
+  const onClickUpdateDetails = async () => {
+
+    const firstname = document.getElementById('firstName').value;
+    const lastname = document.getElementById('lastName').value;
+
+    if(firstname == '' || lastname == ''){
+      alert('Fields cannot be empty.');
+    }else{
+      let updatedob = dateOfBirth == '' ? data?.user.dob : dateOfBirth;
+      let formData = {
+        username : data?.user.username,
+        email : data?.user.email,
+        firstName : firstname,
+        lastName : lastname,
+        dob : updatedob,
+        detail : "updateDetails"
+      }
+      try {
+        const response = await axios.put(
+          'http://localhost:3001/api/changeData',
+          formData,
+        );
+        console.log('Response:', response.data);
+        alert("User Details Changed Successfully");
+        setTimeout(() => {
+          setReloadCount(reloadCount + 1);
+        }, 1000);
+      } catch (error) {
+        alert( error.response.data.error);
+        console.error('Error:', error);
+      }
+    }
+  }
+
+  const onClickLogout = () => {
+    navigate('/login');
+  }
+
+  const onClickDelete = async () => {
+
+    const isConfirmed = window.confirm("Are you sure you want to delete?");
+
+    // Check user's response
+    if (isConfirmed) {
+      console.log("Deleting...");
+
+      try {
+        const response = await axios.delete(
+          'http://localhost:3001/api/deleteAccount',
+          {
+            data: {
+              username: data?.user.username,
+              email: data?.user.email,
+            }
+          }
+        );
+        console.log('Response:', response.data);
+        alert("User Deleted Successfully");
+        setTimeout(() => {
+          navigate('/login');
+        }, 1000);
+      } catch (error) {
+        alert( error.response.data.error);
+        console.error('Error:', error);
+      }
+    } else {
+      console.log("Delete cancelled.");
+    }
+  }
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setProfileImage(file);
+  };
+
+  const handleUpload = async () => {
+    console.log("Selected image:", profileImage);
+
+    const imageUrl = profileImage ? await uploadFile("image", profileImage ) : null;
+    await updateProfileImage(imageUrl);
+  };
+
+  const updateProfileImage = async (imageUrl) => {
+    try {
+
+      let formData = {
+        username : data?.user.username,
+        email : data?.user.email,
+        url: imageUrl,
+        detail : "updateProfileImage"
+      }
+
+      const response = await axios.put('http://localhost:3001/api/changeData', formData);
+
+      alert("User Image Updated Successfully");
+      setTimeout(() => {
+        imageInputRef.current.value = '';
+        setReloadCount(reloadCount + 1);
+      }, 1000);
+      
+
+    }catch (error) {
+      console.error("Error creating game:", error);
+      alert(error);
+    }
+  }
+  
+
+  const uploadFile = async (type, file) => {
+    const data = new FormData();
+    data.append("file", file);
+    data.append(
+      "upload_preset",
+      type === "image" ? "userProfile_Preset" : "Stream_Preset"
+    );
+
+    try {
+      let cloudName = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
+      console.log("Cloudinary cloud name:", cloudName);
+      let resourceType = type === "image" ? "image" : "video";
+      let api = `https://api.cloudinary.com/v1_1/dg8cpnx1m/${resourceType}/upload`;
+
+      const res = await axios.post(api, data);
+      const { secure_url } = res.data;
+      console.log(`${type} uploaded successfully:`, secure_url);
+
+      return secure_url;
+    } catch (error) {
+      console.error(
+        "Error uploading file to Cloudinary:",
+        error.response?.data
+      );
+      throw new Error("Failed to upload file to Cloudinary");
+    }
+  };
+
+  const generatePDF = () => {
+
+    const input = document.getElementById('pdf-table');
+  
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      const imgWidth = 210;
+      const pageHeight = 297;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+      let position = 0;
+  
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+  
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+  
+      pdf.save('All Users.pdf');
+    });
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      const userEmail = data?.user.email;
+      if (userEmail) { 
+        console.log(userEmail);
+
+        const fetchData = async () => {
+          try {
+            const response = await axios.get('http://localhost:3001/api/getuser?email=' + userEmail);
+            setUserData(response.data);
+          } catch (error) {
+            console.error("Error fetching data:", error);
+            alert(error.message);
+          }
+        };
+
+        fetchData();
+      }
+  }, 1000);
+  }, [reloadCount,selectedDiv]);
+  
+
+  const { data, isLoading } = useSWR('api/me');
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/api/allUsers');
+        setAllUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        alert(error.message);
+      }
+    };
+
+    fetchData();
+
+  }, [])
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedField, setSelectedField] = useState('username');
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value.toLowerCase());
+  };
+
+  const handleFieldChange = (event) => {
+    setSelectedField(event.target.value);
+  };
+
+  const filteredData = allUserData.filter(user => {
+    if (!user[selectedField]) return false;
+    return user[selectedField].toString().toLowerCase().includes(searchQuery);
+  });
+  
+
+
+
+
+  // const handleDeleteCloseSuccessPopup = () => {
+  //   setDeleteSuccessMessagechecked(false);
+  // };
 
   return (
     <div>
-      <Header navid="home" />
+      <Header navid="home" key={`${reloadCount}-${selectedDiv}`}/>
       <div className="h-1/4 p-8 flex flex-row justify-center">
         <div className="flex justify-center">
-          <img
-            className="h-32 w-32"
-            src="https://img.icons8.com/ios-filled/100/FD7E14/user-male-circle.png"
-            alt="user-male-circle"
+        <img
+            className="h-32 w-32 rounded-full"
+            src={userData ? (userData.image !== '' ? userData.image : 'https://cdn-icons-png.flaticon.com/512/149/149071.png') : 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
+            alt="user"
           />
         </div>
         <div className="px-5">
-          <h1 className="font-bold text-[40px] text-white">
-            Jonathan durairaj
+        <h1 className="font-bold text-[40px] text-white">
+          {userData ? userData?.firstname : ''}
+          {userData ? userData?.lastname : ''}
           </h1>
           <span className="bg-gradient-to-b from-[#FF451D] to-[#FE7804] text-white px-2 py-1 rounded-2xl text-[14px]">
             Primium
@@ -297,6 +601,26 @@ export default function Myaccount() {
               Settings
             </label>
           </div>
+          {data?.user.type == 'admin' ? 
+            <div
+              className={`py-2 px-5 rounded-t-[10px]  ${
+                selectedDiv === 'AllUsers'
+                  ? 'bg-gradient-to-t from-[#FF451D] to-[#FE7804]'
+                  : ''
+              }`}
+            >
+              <input type="checkbox" id="AllUsers" />
+              <label
+                htmlFor="AllUsers"
+                className="text-white font-medium hover:text-[#FF451D] cursor-pointer"
+                onClick={() => handleDivClick('AllUsers')}
+              >
+                All Users
+              </label>
+            </div>
+            :
+            null
+          }
         </div>
       </div>
 
@@ -750,8 +1074,131 @@ export default function Myaccount() {
       {/* ######################### Settings ########################   */}
       {selectedDiv === "Settings" && (
         <div className="w-11/12 mx-auto mt-3">
-          <h1>Settings</h1>
+        <div className="text-white font-sans p-8">
+          <div className="max-w-4xl mx-auto">
+              <h1 className="text-2xl font-bold mb-6">My Profile</h1>
+              
+              <div className="mb-8">
+                  <div className="flex justify-between items-center mb-4">
+                      <div>
+                          <label htmlFor="firstName" className="block text-sm font-medium">First Name</label>
+                          <input onChange={(e)=> setFirstName(e.target.value)} type="text" id="firstName" value={firstName == '' ? userData?.firstname : firstName} className={inputClasses} />
+                      </div>
+                      <div>
+                          <label htmlFor="lastName" className="block text-sm font-medium">Last Name</label>
+                          <input onChange={(e)=> setLastName(e.target.value)} type="text" id="lastName" value={lastName == '' ? userData?.lastname : lastName} className={inputClasses} />
+                      </div>
+                  </div>
+                  <div className="mb-6">
+                      <label htmlFor="email" className="block text-sm font-medium">Email</label>
+                      <input readOnly type="email" id="email" defaultValue={data ? data?.user.email : ''} className={inputClasses} />
+                  </div>
+                  <div className="mb-6">
+                      <label htmlFor="dateOfBirth" className="block text-sm font-medium">Date of Birth</label>
+                      <input onChange={(e)=> setDateOfBirth(e.target.value)} type="date" id="dateOfBirth" value={dateOfBirth == '' ? userData?.dob : dateOfBirth} className={inputClasses} max={getCurrentDate()}/>
+                  </div>
+                  <button onClick={onClickUpdateDetails} className={`mt-4 bg-red-600 hover:bg-red-700 ${buttonClasses}`}>Update Details</button>
+              </div>
+
+              <hr/><br/>
+
+              <div className="mb-8">
+                  <h2 className="text-xl font-semibold mb-4">Change Password</h2>
+                  <div className="flex justify-between items-center">
+                      <div className="w-full mr-4">
+                          <label htmlFor="newPassword" className="block text-sm font-medium">Create Password</label>
+                          <input type="password" id="newPassword" className={inputClasses}/>
+                      </div>
+                      <div className="w-full ml-4">
+                          <label htmlFor="confirmPassword" className="block text-sm font-medium">Re-Enter Password</label>
+                          <input type="password" id="confirmPassword" className={inputClasses}/>
+                      </div>
+                  </div>
+                  <button onClick={onClickChangePass} className={`mt-8 bg-red-600 hover:bg-red-700 ${buttonClasses}`}>Change Password</button>
+              </div>
+
+              <hr/><br/>
+                <h2 className="text-xl font-semibold mb-4">Change profile picture</h2>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className='border border-orange-600 text-white p-2'
+                  ref={imageInputRef}
+                />
+                <button onClick={handleUpload} className='bg-green-600 hover:bg-green-700 ml-10 text-white font-bold py-2 px-4 rounded-lg'>Upload Image</button>
+                <br/><br/>
+              <hr/><br/>
+
+              <button onClick={onClickLogout} className={`bg-red-600 hover:bg-red-700 ${buttonClasses}`}>Log Out</button>
+              <button onClick={onClickDelete} className={`bg-red-600 hover:bg-red-700 ml-10 text-white font-bold py-2 px-4 rounded-lg`}>Delete Account</button>
+          </div>
         </div>
+      </div>
+      )}
+     {/* ######################### All Users ########################   */}
+     {selectedDiv === 'AllUsers' && (
+      <div className="w-11/12 mx-auto mt-5">
+      <h1 className="text-white text-2xl font-bold mb-6">All Users</h1>
+      <div className="mb-8 flex gap-4 items-center">
+  <button onClick={generatePDF} className="float-right bg-gradient-to-tr from-[#FF451D] to-[#FE7804] px-4 py-2 text-[18px] font-semibold rounded-lg text-white">
+    Generate PDF
+  </button>
+
+  <input
+    type="text"
+    className="bg-[#262628] text-[#FE7804] rounded-2xl flex-grow px-4 py-2 rounded-lg placeholder-[#FE7804] h-10 text-white  px-3 py-2"
+    placeholder="Search..."
+    onChange={handleSearchChange}
+  />
+
+<select onChange={handleFieldChange} onFocus={(e) => e.target.style.backgroundColor = '#ff7f50'} // Change to your desired color on focus
+  onBlur={(e) => e.target.style.backgroundColor = '#FF451D'}  // Reset to default color on blur
+  style={{
+    padding: '8px 16px', 
+    borderRadius: '12px',
+    backgroundImage: 'linear-gradient(to top right, #FF451D, #FE7804)',
+    height: '40px', 
+    color: 'white',
+    borderColor: '#ddd' // Default border color, change as needed
+  }} className="px-4 py-2 rounded-lg bg-gradient-to-tr from-[#FF451D] to-[#FE7804] h-10 text-white">
+    <option value="username">Username</option>
+    <option value="email">Email</option>
+    <option value="firstname">First Name</option>
+    <option value="lastname">Last Name</option>
+    <option value="dob">DOB</option>
+    <option value="accountType">Role</option>
+  </select>
+</div>
+
+      
+      <div id="pdf-table" className="overflow-x-auto">
+        <table className="table-auto w-full border-collapse border border-gray-800">
+          <thead>
+            <tr>
+              <th className="border px-4 py-2 border-[#1F2937] bg-gradient-to-tr from-[#FF451D] to-[#FE7804] text-white">Username</th>
+              <th className="border px-4 py-2 border-[#1F2937] bg-gradient-to-tr from-[#FF451D] to-[#FE7804] text-white">Email</th>
+              <th className="border px-4 py-2 border-[#1F2937] bg-gradient-to-tr from-[#FF451D] to-[#FE7804] text-white">First Name</th>
+              <th className="border px-4 py-2 border-[#1F2937] bg-gradient-to-tr from-[#FF451D] to-[#FE7804] text-white">Last Name</th>
+              <th className="border px-4 py-2 border-[#1F2937] bg-gradient-to-tr from-[#FF451D] to-[#FE7804] text-white">DOB</th>
+              <th className="border px-4 py-2 border-[#1F2937] bg-gradient-to-tr from-[#FF451D] to-[#FE7804] text-white">Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((user) => (
+              <tr key={user._id}>
+                <td className="border px-4 py-2 bg-[#262628] text-white border-[#1F2937]">{user.username}</td>
+                <td className="border px-4 py-2 bg-[#262628] text-white border-[#1F2937]">{user.email}</td>
+                <td className="border px-4 py-2 bg-[#262628] text-white border-[#1F2937]">{user.firstname}</td>
+                <td className="border px-4 py-2 bg-[#262628] text-white border-[#1F2937]">{user.lastname}</td>
+                <td className="border px-4 py-2 bg-[#262628] text-white border-[#1F2937]">{user.dob}</td>
+                <td className="border px-4 py-2 bg-[#262628] text-white border-[#1F2937]">{user.accountType}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
       )}
 
       <Footer />
